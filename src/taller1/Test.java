40e2a3b2aa759834186463e8ea06fac8dda98e7c
pelesit <juanjo21@gmail.com>
2015-04-12 12:26:23 -0500

    Se anexan las clases de la solucion propuesta

--- src/taller1/Test.java
+++ src/taller1/Test.java
@@ -1,5 +1,18 @@
 package taller1;
+
+import udem.pds.taller1.io.base.Lector;
+
 /*esto es una pruba jj */
 public class Test {
+	Test(){
+		
+		Lector k = new LectorAcrhivoMayusculas("hola");
+		
+		k= new LectorArchivoSinEspacios("hola");
+		
+		
+		
+		
+	}
 
 } 
